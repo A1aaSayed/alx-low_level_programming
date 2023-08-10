29c4f97e5d788e23aa494f0edde2054a88ea07b4
Alaa Sayed <alaa02.sayed02@gmail.com>
2023-08-10 09:00:17 -0700

    m

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Desctiption: A program to print with puts function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void) 
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
