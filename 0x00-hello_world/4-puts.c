f2dc377b4d6355db3d0e4085c0a00b7f87bcb8bf
Alaa Sayed <alaa02.sayed02@gmail.com>
2023-08-10 09:33:35 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Desctiption: A program to print with puts function
- *
- * Return: Always 0 (Success)
-*/
-
-int main(void) 
+ * main - Entry point.
+ * Description: A C program that print py using puts function
+ * Return: Always 0.
+ */
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
