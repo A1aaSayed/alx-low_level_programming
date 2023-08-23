34bab41d1e287023a07b8f5864616667d36a6ad6
root <root@Alaa.localdomain>
2023-08-23 23:45:57 +0300

    strncat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,13 +9,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-int c, ch;
+	int c, ch;
 
-c = 0;
-while (dest[0])
-c++;
-for (ch = 0; src[ch]; ch++)
-dest[c++] = src[ch];
+	c = 0;
+	while (dest[0])
+		c++;
+	for (ch = 0; src[ch]; ch++)
+		dest[c++] = src[ch];
 
-return (0);
+	return (0);
 }
