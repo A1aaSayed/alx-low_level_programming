401520f6da2fef87bb1c0a3720376971120db0fc
root <root@Alaa.localdomain>
2023-08-23 23:37:41 +0300

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - concate two strings
+ * @dest: string
+ * @src: string
+ * Return: Always 0
+*/
+
+char *_strcat(char *dest, char *src)
+{
+int c, ch;
+
+c = 0;
+while (dest[0])
+c++;
+for (ch = 0; src[ch]; ch++)
+dest[c++] = src[ch];
+
+return (0);
+}
