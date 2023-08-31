d60d07aebbbd585ce09bdff078b2c72fddf7ddee
Alaa Sayed <alaa02.sayed02@gmail.com>
2023-08-31 08:51:22 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,11 +6,21 @@
 */
 int is_prime_number(int n)
 {
+	bool is_prime = true;
+
 	if (n <= 1)
-		return (0);
-	for (int i = 2; i * i <= n; i++)
+		is_prime = false;
+	else
 	{
-		if (n % i == 0)
-		return (0);
+		for (int i = 2; i * i <= n; i++)
+		{
+			if (n % i == 0)
+				is_prime = false;
+		}
 	}
+
+	if (is_prime)
+		return (1);
+	else
+		return (0);
 }
