38943f4ff340e673309a77354bd3508024928751
Alaa Sayed <alaa02.sayed02@gmail.com>
2023-08-31 15:18:01 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,31 +1,29 @@
 #include "main.h"
 
 /**
- * check_prime - check for prime numbers
- * @x: integer
- * @y: intteger
- * Return: 1
-*/
-
-int check_prime(int x, int y)
+ * check - checks to see if number is prime
+ * @a:int
+ * @b:int
+ * Return:int
+ */
+int check(int a, int b)
 {
-	if (y < 2 || y % x == 0)
+	if (b < 2 || b % a == 0)
 		return (0);
-	else if (x > y / 2)
+	else if (a > b / 2)
 		return (1);
 	else
-		return (check_prime(x + 1, b));
+		return (check(a + 1, b));
 }
 
 /**
- * is_prime_number - find prime numbers
- * @n: int
- * Return: 1
-*/
-
+ * is_prime_number - states if number is prime
+ * @n:int
+ * Return:int
+ */
 int is_prime_number(int n)
 {
 	if (n == 2)
 		return (1);
-	return (check_prime(2, n));
+	return (check(2, n));
 }
