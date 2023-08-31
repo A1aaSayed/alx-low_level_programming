711f27c74914dd74b2252f461a06db3b47093eca
Alaa Sayed <alaa02.sayed02@gmail.com>
2023-08-31 09:08:46 -0700

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,18 @@
 #include "main.h"
+/**
+ * sqrt - check sqrt
+ * @n: int
+ * @i: int
+ * Return: sqrt or -1
+*/
+int sqrt(int n, int i)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return sqrt(n, i + 1);
+}
 
 /**
  * _sqrt_recursion - find sqrt root for number
@@ -8,5 +22,7 @@
 
 int _sqrt_recursion(int n)
 {
-
+	if (n < 0)
+		return (-1);
+	return sqrt(n, 0);
 }
