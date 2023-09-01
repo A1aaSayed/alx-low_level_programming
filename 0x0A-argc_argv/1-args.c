3d5c6295e878690920a28c7622872b9452da982b
root <root@Alaa.localdomain>
2023-09-02 00:00:30 +0300

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of args
+ * @argv: args
+ * Return: 0
+*/
+
+int main(int argc, char **argv)
+{
+	printf("%s\n", argc);
+	return (0);
+}
