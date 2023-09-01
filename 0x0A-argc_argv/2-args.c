b75addbbb986ecef61b291b766ebc371d8cd1817
root <root@Alaa.localdomain>
2023-09-02 00:07:45 +0300

    args

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-/***/
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: arguments
+ * Return: 0
+*/
 
 int main(int argc, char **argv)
 {
-	for (int i = 0; i < argc; i++)
+	int i;
+
+	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 	return (0);
 }
