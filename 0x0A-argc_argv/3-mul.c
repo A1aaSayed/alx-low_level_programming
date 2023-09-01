945655a99af58b99736524b98b4c7ff11c6dbf6d
root <root@Alaa.localdomain>
2023-09-02 00:43:28 +0300

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,17 +10,18 @@
 
 int main(int argc, char **argv)
 {
-	int num1, num2, result;
+	int num1, num2;
 
 	if (argc < 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	result = num1 * num2;
-	printf("%d\n", result);
+
+	printf("%d\n", num1 * num2);
 
 	
 	return (0);
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,21 +10,5 @@
 
 int main(int argc, char **argv)
 {
-	int i, j, sum = 0;
 
-	for (i = 1; i < agrc; i++)
-	{
-		for (j = 0; argv[i][j]; j++)
-		{
-			if (!isdigit(argv[i][j]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-		sum += atoi(argv[i]);
-	}
-
-	printf("%d\n", sum);
-	return (0);
 }
