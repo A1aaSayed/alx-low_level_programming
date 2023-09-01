9ec4ec16d6398053e3676e9fb8caa6be1e094b30
root <root@Alaa.localdomain>
2023-09-02 00:56:57 +0300

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,5 +10,23 @@
 
 int main(int argc, char **argv)
 {
+	int i, j, sum = 0;
 
+	for (i = 1; i < argc; i++)
+	{
+		if (argc < 2)
+			printf("0\n");
+		for ( j = 0; argv[i][j]; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
 }
