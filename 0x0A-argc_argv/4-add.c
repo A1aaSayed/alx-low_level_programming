5712f4314ee720e6e501854cdbd4a4b5814b2904
root <root@Alaa.localdomain>
2023-09-02 01:28:27 +0300

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,10 +14,7 @@ int main(int argc, char **argv)
 	for (i = 1; i < argc; i++)
 	{
 		if (argc < 2)
-		{
 			printf("0\n");
-			return (0);
-		}
 		for (j = 0; argv[i][j]; j++)
 		{
 			if (argv[i][j] < '0' || argv[i][j] > '9')
