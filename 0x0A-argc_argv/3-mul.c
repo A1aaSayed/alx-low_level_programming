3583a1a46a0801ba493713c51fdb3fb4e3dbcced
root <root@Alaa.localdomain>
2023-09-02 00:25:23 +0300

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,17 +8,18 @@
 
 int main(int argc, char **argv)
 {
-	int result;
+	int num1, num2, result;
 
-	if (argc == 3)
-	{
-		result = argc[1] * argc[2];
-		printf("%s\n", result);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	result = num1 * num2;
+	printf("%d\n", result);
+
+	
 	return (0);
 }
