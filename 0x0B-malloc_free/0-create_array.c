efa0636ce74c0cb3edee74f6b8c792ae66d518d9
root <root@Alaa.localdomain>
2023-09-06 00:19:15 +0300

    create array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -22,6 +22,7 @@ char *create_array(unsigned int size, char c)
 	while (i < size)
 	{
 		arr[i] = c;
+		i++;
 	}
 	return (arr);
 }
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+
+/***/
+
+char *_strdup(char *str)
+{
+	char *s;
+
+	if (s == NULL)
+		return (NULL);
+}
