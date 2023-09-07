6c2594cff878dc57220eba13fee4b759a653a2ac
root <root@Alaa.localdomain>
2023-09-07 23:05:10 +0300

    nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -24,8 +24,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (len2 = 0; s2[len2]; len2++)
 		;
 
-	if (n >= len2)
-		n = len2;
+	if (len2 >= n)
+		len2 = n;
 
 	s = malloc(sizeof(char) * (len1 + n + 1));
 	if (s == NULL)
