2a6f105fa57f74bbddb089c42825203c1f0b94be
root <root@Alaa.localdomain>
2023-09-08 00:10:34 +0300

    array range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * array_range - creates an array of ints
+ * @min: min value in arr
+ * @max: max value in arr
+ * Return: a pointer to the new array
+*/
+
+int *array_range(int min, int max)
+{
+	int *arr;
+	int i, len;
+
+	if (min > max)
+		return (NULL);
+
+	len = max - min + 1;
+
+	arr = malloc(sizeof(int) * len);
+
+	if (arr == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		a[i] = min + i;
+
+	return (arr);
+}
