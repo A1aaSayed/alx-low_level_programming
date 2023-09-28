f8fbc2e95336e3a31b53f43427a394626cca4428
root <root@Alaa.localdomain>
2023-09-28 13:07:15 +0300

    recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * _puts_recursion - function that prints a string, followed by a new line
  * @s: string
@@ -7,9 +8,7 @@
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
-	{
 		_putchar('\n');
-	}
 	else
 	{
 		_putchar(*s);
