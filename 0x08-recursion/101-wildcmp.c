f84e50da4723aa56caddeb1dffb4d7a16f0a4786
root <root@Alaa.localdomain>
2023-09-28 16:45:55 +0300

    wildcmp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,36 +1,33 @@
 #include "main.h"
 
 /**
- * wildcmp_recursion - compare two strings.
- * @s1: string1
- * @s2: string2
- * @i: left index
- * @j: right index
+ * check - check if two strings are the same
+ * @s1: string_1.
+ * @s2: string_2
+ * @i: left index.
+ * @j: index 
  * Return: 1 or 0
-*/
-
-int wildcmp_recursion(char *s1, char *s2, int i, int j)
+ */
+int check(char *s1, char *s2, int i, int j)
 {
 	if (s1[i] == '\0' && s2[j] == '\0')
 		return (1);
 	if (s1[i] == s2[j])
-		return (wildcmp_recursion(s1, s2, i + 1, j + 1));
+		return (check(s1, s2, i + 1, j + 1));
 	if (s1[i] == '\0' && s2[j] == '*')
-		return (wildcmp_recursion(s1, s2, i, j + 1));
+		return (check(s1, s2, i, j + 1));
 	if (s2[j] == '*')
-		return (wildcmp_recursion(s1, s2, i + 1, j)
-				|| wildcmp_recursion(s1, s2, i, j + 1));
+		return (check(s1, s2, i + 1, j) || check(s1, s2, i, j + 1));
 	return (0);
 }
-
 /**
  * wildcmp - compare two strings
  * @s1: first string
- * @s2: second string
- * Return: 0 or 1
-*/
-
+ * @s2: second string.
+ *
+ * Return: 1 if the two strings are the same
+ */
 int wildcmp(char *s1, char *s2)
 {
-	return (wildcmp(s1, s2, 0, 0));
+	return (check(s1, s2, 0, 0));
 }
