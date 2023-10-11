7a0cf7095083b272a1953e29543fa7fc7fc80b8e
root <root@Alaa.localdomain>
2023-10-11 09:37:42 +0300

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG
-#define DOG
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - dog class
@@ -8,17 +8,15 @@
  * @owner: the dog owner
 */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
 
