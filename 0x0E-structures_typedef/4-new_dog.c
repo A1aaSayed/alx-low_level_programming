71671f66156e76eb0f1fa2c7b7bbef49236f9b30
root <root@Alaa.localdomain>
2023-10-11 11:48:38 +0300

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,7 +2,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 /**
  * new_dog - function that prints a struct dog
  * @name:dog name
@@ -10,34 +9,32 @@
  * @owner:dog owner
  * Return: p address value
 */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 
-	dog = malloc(sizeof(*dog));
-
+	dog = malloc(sizeof(*p));
 	if (dog == NULL)
 		return (NULL);
-
 	(*dog).name = malloc(strlen(name) + 1);
 	(*dog).owner = malloc(strlen(owner) + 1);
 	(*dog).age = age;
 
 	if ((*dog).name == NULL)
 	{
-		free (dog);
+		free(dog);
 		return (NULL);
 	}
+
 	if ((*dog).owner == NULL)
 	{
-		free (dog);
-		free ((*dog).name);
+		free(dog);
+		free((*dog).name);
 		return (NULL);
 	}
 
 	strcpy((*dog).name, name);
 	strcpy((*dog).owner, owner);
 
-	return (dog);	
+	return (dog);
 }
