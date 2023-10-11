758845d33d48276720d6f3cb3e16790f94c0ebd3
root <root@Alaa.localdomain>
2023-10-12 00:35:31 +0300

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - executes a function given as a parameter
+ * @array: the array
+ * @size: the size of the array
+ * @action: a pointer to function
+*/
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array && size && action)
+		for (i = 0; i < size; i++)
+			action(array[i]);
+
+}
