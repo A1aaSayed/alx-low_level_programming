4b3b0a5fe66a023eeb47f17f311ed9043168d482
root <root@Alaa.localdomain>
2023-10-12 01:54:47 +0300

    main

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,10 +1,10 @@
 #include "3-calc.h"
 
 /**
- * op_add - the sum of a and b
+ * op_add - sum of a and b
  * @a: Num 1
  * @b: Num 2
- * Return: returns the sum of a and b
+ * Return: a + b
 */
 int op_add(int a, int b)
 {
@@ -12,10 +12,10 @@ int op_add(int a, int b)
 }
 
 /**
- * op_sub - the difference of a and b
+ * op_sub - subtraction of a and b
  * @a: Num 1
  * @b: Num 2
- * Return: returns the difference of a and b
+ * Return: a - b
 */
 int op_sub(int a, int b)
 {
@@ -23,10 +23,10 @@ int op_sub(int a, int b)
 }
 
 /**
- * op_mul - the product of a and b
+ * op_mul - multiplicaton of a and b
  * @a: Num 1
  * @b: Num 2
- * Return: returns the product of a and b
+ * Return: a * b
 */
 int op_mul(int a, int b)
 {
@@ -34,10 +34,10 @@ int op_mul(int a, int b)
 }
 
 /**
- * op_div - the result of the division of a by b
+ * op_div - division of a by b
  * @a: Num 1
  * @b: Num 2
- * Return: returns the result of the division of a by b
+ * Return: a / b
 */
 int op_div(int a, int b)
 {
@@ -45,10 +45,10 @@ int op_div(int a, int b)
 }
 
 /**
- * op_mod - the remainder of the division of a by b
+ * op_mod - modulas a and b
  * @a: Num 1
  * @b: Num 2
- * Return: returns the remainder of the division of a by b
+ * Return: a % b
 */
 int op_mod(int a, int b)
 {
