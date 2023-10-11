cebe530acd1a668c31dd5cd16ed043c9c12ed394
root <root@Alaa.localdomain>
2023-10-12 02:05:40 +0300

    opcode

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,31 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print opcode
+ * @argc: number of args
+ * @argv: arguments
+ * Return: 0
+*/
+
+int main(int argc, char *argv[])
+{
+	char *p = (char *)main;
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	i = atoi(argv[1]);
+	if (i < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	while (i--)
+		printf("%02hhx%s", *p++, i ? " " : "\n");
+	return (0);
+}
