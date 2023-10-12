1a7c240321e656355576b2d20e6c003d45d6395b
root <root@Alaa.localdomain>
2023-10-13 00:36:45 +0300

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -13,7 +13,7 @@ void print_all(const char * const format, ...)
 
 	va_start(list, format);
 	i = 0;
-	while (format && format[i])
+	while (format && format[i] != '\0')
 	{
 		switch (format[i])
 		{
