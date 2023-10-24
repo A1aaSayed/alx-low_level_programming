c06520abea3df2526d56636dccb054de70aa6dee
A1aaSayed <alaa02.sayed02@gmail.com>
2023-10-24 15:01:35 -0700

    pop list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * pop_listint - delete the head node
+ * @head: first node
+ * Return: data of head
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *ptr = *head;
+	int data;
+
+	if (!*head)
+		return (0);
+
+	data = p->n;
+
+	*head = (*head)->next;
+	ptr->next = NULL;
+	free(p);
+	return (n);
+}
