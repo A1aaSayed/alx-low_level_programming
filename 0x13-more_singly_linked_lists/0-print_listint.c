8dcefc7349532c587cc4564dc8c322523b182558
A1aaSayed <alaa02.sayed02@gmail.com>
2023-10-24 13:29:27 -0700

    print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,10 +8,11 @@
 
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *ptr = h;
+	const listint_t *ptr;
 	int count = 0;
 
-	while (ptr != NULL)
+	ptr = h;
+	while (ptr)
 	{
 		printf("%d\n", ptr->n);
 		count++;
