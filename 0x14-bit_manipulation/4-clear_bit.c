9aa41dc97664cf179c0ff7ff3736ea35257177e5
A1aaSayed <alaa02.sayed02@gmail.com>
2023-10-27 02:47:00 -0700

    clear

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,12 +10,9 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int i;
-
 	if (index > (sizeof(unsigned long int) * 8 - 1))
 		return (-1);
 
-	i = ~(1 << index);
-	*n = *n & i;
+	*n = *n & ~(1 << index);
 	return (1);
 }
