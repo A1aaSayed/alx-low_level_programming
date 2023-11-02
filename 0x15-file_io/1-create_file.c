f440cb3ed6c0599ede4d1c9a57806543867d53e0
A1aaSayed <alaa02.sayed02@gmail.com>
2023-11-02 15:56:58 -0700

    create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,7 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	ssize_t write_result;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
@@ -34,7 +34,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	if (!text_content)
 		text_content = "";
 	write_result = write(fd, text_content, _strlen(text_content));
 	if (write_result == -1)
