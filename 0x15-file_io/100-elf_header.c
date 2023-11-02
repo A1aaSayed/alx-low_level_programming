047a734bea0882c1703761260ab0b48c3987364f
A1aaSayed <alaa02.sayed02@gmail.com>
2023-11-02 16:40:38 -0700

    header

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: number of args
+ * @argv: args
+ * Return: --
+ */
+int main(int argc, char *argv[])
+{
+	int fd;
+
+	if (argc != 2)
+	{
+		write(STDERR_FILENO, "Usage: elf_header elf_filename\n", 31);
+		exit(98);
+	}
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
+	{
+		write(STDERR_FILENO, "Error: Can't read from file\n", 28);
+		exit(98);
+	}
+	exit(98);
+}
