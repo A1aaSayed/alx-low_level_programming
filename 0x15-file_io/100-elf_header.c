63864f08274c4420b4fa37b6737d879fed389582
A1aaSayed <alaa02.sayed02@gmail.com>
2023-11-02 16:45:02 -0700

    m

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -14,6 +14,7 @@ int main(int argc, char *argv[])
 		write(STDERR_FILENO, "Usage: elf_header elf_filename\n", 31), exit(98);
 
 	fd = open(argv[1], O_RDONLY);
+
 	if (fd == -1)
 		write(STDERR_FILENO, "Error: Can't read from file\n", 28), exit(98);
 	exit(98);
