0c0cbc20ac57f61acab1ef96fcb66bc176a3f32a
root <root@AlaaSayed.localdomain>
2023-12-12 01:05:32 +0200

    dynamic

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P https://github.com/A1aaSayed/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libgiga.so
-export LS_PRELOAD=/tmp/libgiga.so
+export LS_PRELOAD=/libgiga.so
