848de7fe6052dc999ceedb80c0237c3906487c13
root <root@AlaaSayed.localdomain>
2023-12-13 10:57:47 +0200

    insert

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - insert a node at a specific index
+ * @h: pointer to the head of the node
+ * @idx: idx in which the node will be inserted
+ * @n: data of the node
+ * Return: pointer to the new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *curr = *h;
+	unsigned int i = 0;
+
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+
+	while (curr && i < idx)
+	{
+		curr = curr->next;
+		i++;
+	}
+
+	if (idx == 0)
+	{
+		free(new_node);
+		return (add_dnodeint(h, n));
+	}
+	else if (curr)
+	{
+		new_node->next = curr;
+		new_node->prev = curr->prev;
+		curr->prev->next = new_node;
+		curr->prev = new_node;
+	}
+	else
+	{
+		free(new_node);
+		return (add_dnodeint_end(h, n));
+	}
+	return (new_node);
+}
---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - delete a specific node at given index
+ * @head: pointer to the head of the node
+ * @index: index of the node that will be deleted
+ * Return: 1 on success -1 on fail
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+}
