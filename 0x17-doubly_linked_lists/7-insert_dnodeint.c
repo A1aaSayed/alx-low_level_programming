28b93688ac9de35aced40cf83512fb8cc6a7364f
root <root@AlaaSayed.localdomain>
2023-12-13 11:23:15 +0200

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,32 +11,38 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new_node = malloc(sizeof(dlistint_t)), *curr = *h;
 	unsigned int i = 0;
 
+	if (!h)
+		return (NULL);
+
 	if (!new_node)
 		return (NULL);
 
 	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
 
-	while (curr && i < idx)
+	if (idx == 0)
+	{
+		return(add_dnodeint(h, n));
+	}
+
+	while (i < idx - 1 && curr)
 	{
 		curr = curr->next;
 		i++;
 	}
 
-	if (idx == 0)
-	{
-		return (add_dnodeint(h, n));
-	}
-	else if (curr)
-	{
-		new_node->next = curr;
-		new_node->prev = curr->prev;
-		curr->prev->next = new_node;
-		curr->prev = new_node;
-	}
-	else
+	if (!curr)
 	{
 		free(new_node);
-		return (add_dnodeint_end(h, n));
+		return NULL;
 	}
+
+	new_node->next = curr->next;
+	new_node->prev = curr;
+	if (curr->next)
+		curr->next->prev = new_node;
+	curr->next = new_node;
+
 	return (new_node);
 }
