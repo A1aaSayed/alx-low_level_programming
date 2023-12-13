a4791bf02072633fe1fe866daae54c88011d6b5e
root <root@AlaaSayed.localdomain>
2023-12-13 11:24:50 +0200

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -32,10 +32,10 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		i++;
 	}
 
-	if (!curr)
+	if (!curr->next)
 	{
 		free(new_node);
-		return NULL;
+		return (add_dnodeint_end(h, n));
 	}
 
 	new_node->next = curr->next;
